da15c06bef62fb6136caf375bb44f8286ec1da4d
pascoalkahamba <pascoalkahamba25@gmail.com>
2022-12-27 08:16:42 +0100

    puting content on the page

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,30 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <h1>Hello World!</h1>
+      <header className="header">
+        <h1>Minha Empresa</h1>
+      </header>
+      <section className="container">
+        <div className="title">
+          <h1>Produtos</h1>
+          <span>Lista de produtos da minha loja</span>
+        </div>
+
+        <div className="form">
+          <div className="data">
+            <label htmlFor="nome">Produto</label>
+            <input type="text" id="nome" />
+          </div>
+          <div className="data">
+            <label htmlFor="value">Valor</label>
+            <input type="text" id="value" />
+          </div>
+          <div className="buttons">
+            <button>enviar</button>
+            <button>cancelar</button>
+          </div>
+        </div>
+      </section>
     </div>
   );
 }
